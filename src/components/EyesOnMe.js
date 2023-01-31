95cfdfc5de20fd2d53f7456177ec40c43cc3214f
George Elkik <gelkik@gmail.com>
2023-01-31 13:08:53 -0500

    final

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function handleFocus(){
+        console.log('Good!')
+    }
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
